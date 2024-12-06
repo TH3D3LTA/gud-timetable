508896bcedd01c858db543851b434b8aafeadf2c
Daniil <140918353+TH3D3LTA@users.noreply.github.com>
2024-12-06 18:44:35 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import LessonCard from "./components/LessonCard.vue";
+let currentDate = new Date();
+let toDay = currentDate.getDate();
+let toMonth = currentDate.getMonth();
+let toDayWeek = currentDate.getDay();
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+let days = [
+    'вс',
+    'пн',
+    'вт',
+    'ср',
+    'чт',
+    'пт',
+    'сб'
+];
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+let weekEven = [
+    'нечётная',
+    'чётная'
+];
+
+let month = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря"
+];
+
+let today = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getTime();
+let now = new Date().getTime();
+let week = Math.ceil((now - today) / (1000 * 60 * 60 * 24 * 7));
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+    <section class="flex flex-col bg-gradient-to-b from-black to-white margin-0 h-screen overflow-hidden items-center">
+        <div class="flex flex-col py-16 items-center">
+            <p class="prata text-white text-[32px] w-fit">{{toDay}} {{month[toMonth]}}, {{days[toDayWeek]}}</p>
+            <p class="inter text-white text-[16px] font-regular w-fit">{{weekEven[week]}} неделя</p>
+        </div>
+        <div class="py-2 bg-[--container-dark] flex flex-col gap-2 text-white h-full p-2 rounded-t-[24px] w-full overflow-y-auto">
+            <LessonCard />
+            <LessonCard />
+            <LessonCard />
+            <LessonCard />
+        </div>
+    </section>
+</template>
---
+++ src/components/LessonCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-row gap-4 rounded-2xl bg-[--card-dark] p-4 items-center">
+        <p class="ibm text-[40px]">1</p>
+        <div class="flex flex-col min-w-fit">
+            <p class="inter font-bold text-[16px]">Математический анализ</p>
+            <div class="flex flex-row gap-4">
+                <p>1-118</p>
+                <p>Гришанина Г.Э.</p>
+            </div>
+        </div>
+        <div class="w-full" />
+        <div class="ibm flex flex-col">
+            <p>09:00</p>
+            <p>10:30</p>
+        </div>
+    </div>
+</template>
